fff5c499313197c6b36cbfb74915229b76ca40c3
chenpi666 <95206458+chenpi666@users.noreply.github.com>
2023-05-26 00:21:15 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -75,14 +75,14 @@ function Install_ct() {
   Installation_dependency
   check_file
   check_sys
-  rm -rf gost-linux-"$bit"-"$ct_new_ver".gz
-  wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz
-  gunzip gost-linux-"$bit"-"$ct_new_ver".gz
-  mv gost-linux-"$bit"-"$ct_new_ver" gost
-  mv gost /usr/bin/gost
-  chmod -R 777 /usr/bin/gost
-  wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
-  mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
+    rm -rf gost-linux-"$bit"-"$ct_new_ver".gz
+    wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz
+    gunzip gost-linux-"$bit"-"$ct_new_ver".gz
+    mv gost-linux-"$bit"-"$ct_new_ver" gost
+    mv gost /usr/bin/gost
+    chmod -R 777 /usr/bin/gost
+    wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   systemctl enable gost && systemctl restart gost
 }
 function Restart_ct() {
@@ -91,10 +91,245 @@ function Restart_ct() {
   writeconf
   conflast
   systemctl restart gost
-  echo "已重读配置并重启"
+}
+function writerawconf() {
+  echo $flag_a"/""$flag_b""#""$flag_c""#""$flag_d" >>$raw_conf_path
+}
+function rawconf() {
+  read_protocol
+  read_s_port
+  read_d_ip
+  read_d_port
+  writerawconf
+}
+function eachconf_retrieve() {
+  d_server=${trans_conf#*#}
+  d_port=${d_server#*#}
+  d_ip=${d_server%#*}
+  flag_s_port=${trans_conf%%#*}
+  s_port=${flag_s_port#*/}
+  is_encrypt=${flag_s_port%/*}
+}
+function confstart() {
+  echo "{
+    \"Debug\": true,
+    \"Retries\": 0,
+    \"ServeNodes\": [" >>$gost_conf_path
+}
+function multiconfstart() {
+  echo "        {
+            \"Retries\": 0,
+            \"ServeNodes\": [" >>$gost_conf_path
+}
+function conflast() {
+  echo "    ]
+}" >>$gost_conf_path
+}
+function multiconflast() {
+  if [ $i -eq $count_line ]; then
+    echo "            ]
+        }" >>$gost_conf_path
+  else
+    echo "            ]
+        }," >>$gost_conf_path
+  fi
+}
+function method() {
+  if [ $i -eq 1 ]; then
+    if [ "$is_encrypt" == "nonencrypt" ]; then
+      echo "        \"tcp://:$s_port/$d_ip:$d_port\",
+        \"udp://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnno" ]; then
+      echo "        \"tcp://:$s_port/$d_ip?host=$d_port\",
+        \"udp://:$s_port/$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerno" ]; then
+      echo "        \"tcp://:$s_port?ip=/root/$d_ip.txt&strategy=$d_port\",
+        \"udp://:$s_port?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encrypttls" ]; then
+      echo "        \"tcp://:$s_port\",
+        \"udp://:$s_port\"
+    ],
+    \"ChainNodes\": [
+        \"relay+tls://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encryptws" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+ws://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encryptwss" ]; then
+      echo "        \"tcp://:$s_port\",
+		  \"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+		\"relay+wss://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peertls" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+tls://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerws" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+ws://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerwss" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+wss://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnws" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+ws://$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnwss" ]; then
+      echo "        \"tcp://:$s_port\",
+    	\"udp://:$s_port\"
+	],
+	\"ChainNodes\": [
+    	\"relay+wss://$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "decrypttls" ]; then
+      if [ -d "$HOME/gost_cert" ]; then
+        echo "        \"relay+tls://:$s_port/$d_ip:$d_port?cert=/root/gost_cert/cert.pem&key=/root/gost_cert/key.pem\"" >>$gost_conf_path
+      else
+        echo "        \"relay+tls://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+      fi
+    elif [ "$is_encrypt" == "decryptws" ]; then
+      echo "        \"relay+ws://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "decryptwss" ]; then
+      if [ -d "$HOME/gost_cert" ]; then
+        echo "        \"relay+wss://:$s_port/$d_ip:$d_port?cert=/root/gost_cert/cert.pem&key=/root/gost_cert/key.pem\"" >>$gost_conf_path
+      else
+        echo "        \"relay+wss://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+      fi
+    elif [ "$is_encrypt" == "ss" ]; then
+      echo "        \"ss://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "socks" ]; then
+      echo "        \"socks5://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "http" ]; then
+      echo "        \"http://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    else
+      echo "config error"
+    fi
+  elif [ $i -gt 1 ]; then
+    if [ "$is_encrypt" == "nonencrypt" ]; then
+      echo "                \"tcp://:$s_port/$d_ip:$d_port\",
+                \"udp://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerno" ]; then
+      echo "                \"tcp://:$s_port?ip=/root/$d_ip.txt&strategy=$d_port\",
+                \"udp://:$s_port?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnno" ]; then
+      echo "                \"tcp://:$s_port/$d_ip?host=$d_port\",
+                \"udp://:$s_port/$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encrypttls" ]; then
+      echo "                \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+tls://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encryptws" ]; then
+      echo "                \"tcp://:$s_port\",
+	            \"udp://:$s_port\"
+	        ],
+	        \"ChainNodes\": [
+	            \"relay+ws://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "encryptwss" ]; then
+      echo "                \"tcp://:$s_port\",
+		        \"udp://:$s_port\"
+		    ],
+		    \"ChainNodes\": [
+		        \"relay+wss://$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peertls" ]; then
+      echo "                \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+tls://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerws" ]; then
+      echo "                \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+ws://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "peerwss" ]; then
+      echo "                \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+wss://:?ip=/root/$d_ip.txt&strategy=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnws" ]; then
+      echo "                \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+ws://$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "cdnwss" ]; then
+      echo "                 \"tcp://:$s_port\",
+                \"udp://:$s_port\"
+            ],
+            \"ChainNodes\": [
+                \"relay+wss://$d_ip?host=$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "decrypttls" ]; then
+      if [ -d "$HOME/gost_cert" ]; then
+        echo "        		  \"relay+tls://:$s_port/$d_ip:$d_port?cert=/root/gost_cert/cert.pem&key=/root/gost_cert/key.pem\"" >>$gost_conf_path
+      else
+        echo "        		  \"relay+tls://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+      fi
+    elif [ "$is_encrypt" == "decryptws" ]; then
+      echo "        		  \"relay+ws://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "decryptwss" ]; then
+      if [ -d "$HOME/gost_cert" ]; then
+        echo "        		  \"relay+wss://:$s_port/$d_ip:$d_port?cert=/root/gost_cert/cert.pem&key=/root/gost_cert/key.pem\"" >>$gost_conf_path
+      else
+        echo "        		  \"relay+wss://:$s_port/$d_ip:$d_port\"" >>$gost_conf_path
+      fi
+    elif [ "$is_encrypt" == "ss" ]; then
+      echo "        \"ss://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "socks" ]; then
+      echo "        \"socks5://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    elif [ "$is_encrypt" == "http" ]; then
+      echo "        \"http://$d_ip:$s_port@:$d_port\"" >>$gost_conf_path
+    else
+      echo "config error"
+    fi
+  else
+    echo "config error"
+    exit
+  fi
+}
+
+function writeconf() {
+  count_line=$(awk 'END{print NR}' $raw_conf_path)
+  for ((i = 1; i <= $count_line; i++)); do
+    if [ $i -eq 1 ]; then
+      trans_conf=$(sed -n "${i}p" $raw_conf_path)
+      eachconf_retrieve
+      method
+    elif [ $i -gt 1 ]; then
+      if [ $i -eq 2 ]; then
+        echo "    ],
+    \"Routes\": [" >>$gost_conf_path
+        trans_conf=$(sed -n "${i}p" $raw_conf_path)
+        eachconf_retrieve
+        multiconfstart
+        method
+        multiconflast
+      else
+        trans_conf=$(sed -n "${i}p" $raw_conf_path)
+        eachconf_retrieve
+        multiconfstart
+        method
+        multiconflast
+      fi
+    fi
+  done
 }
 
 Install_ct
 echo -e "encryptws/13321#tw1.xiaopistore.cloud#45621\nencryptws/13312#141.98.16.194#45412\nencryptws/45135#27.0.234.10#13313\nencryptws/32114#147.78.247.159#15421\nencryptws/35421#217.145.236.69#23415\nencryptws/53412#89.248.66.36#62345\nencryptws/34561#hk1.654623.xyz#45562\nencryptws/43245#114.29.236.21#22334" > /etc/gost/rawconf
 Restart_ct
-echo net.core.default_qdisc=fq >> /etc/sysctl.conf && echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf && sysctl -p
